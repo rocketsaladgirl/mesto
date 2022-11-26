79e6a266268b6d1f9c9fe65281daa5fc35d5b009
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2022-11-26 20:44:58 +0300

    added popup and js

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+//Обработка процесса открытия и закрытия вкладки popup
+let popup = document.querySelector('.popup');
+let openbutton = document.querySelector('.profile__edit-button');
+let closebutton = document.querySelector('.popup__close-button');
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+}
+openbutton.addEventListener('click', openPopup);
+
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+closebutton.addEventListener('click', closePopup);
+
+//Обработка процесса работы кнопки "Сохрани"
+let formElement = document.querySelector('.popup__form');
+
+let nameInput = document.querySelector('.popup__username');
+let jobInput = document.querySelector('.popup__description');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    const name = document.querySelector('.profile__username');
+    const description = document.querySelector('.profile__description');
+
+    name.textContent = nameInput.value;
+    description.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
